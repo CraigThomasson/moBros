8ec1078860a13277e05529698fe04de3c03fc5c4
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-22 23:24:48 +0000

    pull from alex and add basic user prifile

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from activities.models import Exercise
 
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     exercises = models.ForeignKey(Exercise, on_delete=models.CASCADE)
