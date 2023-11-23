92c8b659bd298b784a033c8453480ae6c778e05b
alexandergrib <alexgrib@hotmail.com>
2023-11-23 20:58:48 +0000

    Created edit activity functionality

--- activities/views.py
+++ activities/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from activities.forms import EditActivityForm
 from activities.models import Activity
+from categories.models import Categories
 from user_profile.models import Profile
 
 
 # Create your views here.
 def activity_list(request):
-    categories = ''
+    categories = Categories.objects.all()
 
     activity_list = Activity.objects.all()
 
     ctx = {
-        categories: categories,
+        'categories': categories,
         'activity_list': activity_list,
     }
 
@@ -20,14 +21,15 @@ def activity_list(request):
 
 
 def edit_activity(request, activity_id):
-    activity = get_object_or_404(Activity, activity_id)
-
+    activity = get_object_or_404(Activity, pk=activity_id)
     form = EditActivityForm(instance=activity)
-
-
+    if request.method == 'POST':
+        form = EditActivityForm(request.POST, instance=activity)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('activities'))
     ctx = {
         'activity': activity,
         'form': form
     }
     return render(request, 'activities/edit_activity.html', ctx)
-
