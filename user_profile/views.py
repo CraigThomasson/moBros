53581c9389c71466a32a919eb7ae072d7dd3dede
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-23 09:38:51 +0000

    set up signup and sign up logic

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import ProfileForm
+from .forms import ProfileForm, UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 @login_required
 def view_profile(request):
@@ -17,3 +18,14 @@ def edit_profile(request):
     else:
         form = ProfileForm(instance=request.user.profile)
     return render(request, 'user_profiles/edit_profile.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user_profiles/register.html', {'form': form})
