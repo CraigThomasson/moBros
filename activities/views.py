8a757f44d73cdb478e495bceda7277f8ff5f4df7
alexandergrib <alexgrib@hotmail.com>
2023-11-23 18:04:12 +0000

    Created activities.html template

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def activity_list(request):
+    categories = ''
+    activity_list = ''
+
+    ctx = {
+        categories: categories,
+        activity_list: activity_list,
+    }
+
+    return render(request, 'activities/activities.html', ctx)
+
--- django_settings/urls.py
+++ django_settings/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('activities/', include(activities.urls)),
     path('user/', include('user_profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
