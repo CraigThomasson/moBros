235383e6eaf75f1f0370e7a76270f6d11ed761e2
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-24 19:39:56 +0000

    add signin page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('register/', views.register, name='register'),
+    path('sign-in/', views.sign_in, name='sign_in'),
 ]
