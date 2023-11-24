992412f69522145f4fb97a312d5e1bc00fa42fd8
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-24 13:19:01 +0000

    update settings for heroku deployment

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -13,6 +13,9 @@ import environ
 import os
 from pathlib import Path
 
+import django_heroku
+django_heroku.settings(locals())
+
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -62,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_settings.urls'
@@ -137,10 +141,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
