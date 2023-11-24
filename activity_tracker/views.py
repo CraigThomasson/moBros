24d666d75e181d57cdb2a7754e6d2f5c62654431
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-24 00:10:31 +0000

    add chart.js to display activity data

--- activity_tracker/views.py
+++ activity_tracker/views.py
@@ -3,6 +3,8 @@ from .models import CompletedActivity
 from activities.models import Activity
 from django.contrib.auth.decorators import login_required
 from datetime import date
+from django.db.models import Count, DateField
+from django.db.models.functions import TruncDate
 
 
 def log_completed_activity(request):
@@ -15,4 +17,14 @@ def log_completed_activity(request):
         CompletedActivity.objects.create(user=request.user, activity=activity, date_completed=date.today())
         return redirect('log_completed_activity')
 
-    return render(request, 'activity_tracker/log_completed_activity.html', {'activities': activities})
+    completed_activities = CompletedActivity.objects.filter(user=request.user)
+    activity_data = completed_activities.annotate(date=TruncDate('date_completed')).values('date').annotate(count=Count('id')).values('date', 'count')
+
+    activity_dates = [activity['date'].strftime("%Y-%m-%d") for activity in activity_data]
+    activity_counts = [activity['count'] for activity in activity_data]
+
+    return render(request, 'activity_tracker/log_completed_activity.html', {
+        'activities': activities,
+        'activity_dates': activity_dates,
+        'activity_counts': activity_counts
+    })
