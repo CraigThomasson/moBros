c2216a7bce1b0ac903147e24cf09ca641abd571d
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-11-25 13:40:05 +0000

    fixing sign up wip

--- user_profile/apps.py
+++ user_profile/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+class YourAppConfig(AppConfig):
+    name = 'your_app_name'
+
+    def ready(self):
+        import your_app_name.signals  # Replace 'your_app_name' with the name of your app
+
 
 class UserProfileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
